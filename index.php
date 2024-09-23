5f12557e905c1da704399739e583ad0d73cc7ef0
mandinha003 <e107ti@etec.sp.gov.br>
2024-09-23 20:36:01 -0300

    aula dada

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula PHP</title>
+</head>
+<body>
+    <form action="script.php" method="post">
+    <label for="nome">Nome</label>
+    <input type="text" name="nome" id="nome">
+    <label for="nota1">nota1</label>
+    <input type="number" name="nota1" id="nota1">
+    <label for="nota2">nota2</label>
+    <input type="number" name="nota2" id="nota2">
+    <label for="nota3">nota3</label>
+    <input type="number" name="nota3" id="nota3">
+    <label for="nota4">nota4</label>
+    <input type="number" name="nota4" id="nota4">
+
+    <button type="submit">Enviar</button>
+    
+
+    </form>
+
+
+
+</body>
+</html>
